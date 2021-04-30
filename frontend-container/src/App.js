ccd885c33b431b816d17e282e1ab1701d1ae2eeb
PenguinJesus <wongjeff80@gmail.com>
2021-04-30 16:34:57 +0100

    first commit

--- frontend-container/src/App.js
+++ frontend-container/src/App.js
@@ -12,21 +12,12 @@ function App() {
   const [data2,setData2] = useState('');
 
   const container1 = 'http://localhost:8080/data';
-  const container2 = 'http://localhost:5000/data2'
   
   const container1Event = () => {
     axios.get(container1)
     .then(resp => {
       console.log(resp);
       setData1(resp.data.body);
-    })
-    .catch(err=>console.log(err))
-  }
-  const container2Event = () => {
-    axios.get(container2)
-    .then(resp => {
-      console.log(resp);
-      setData2(resp.data.body);
     })
     .catch(err=>console.log(err))
   }
@@ -40,16 +31,10 @@ function App() {
           <div>
             <Button variant='primary' onClick={container1Event}>Container 1</Button> {' '}
           </div>
-          <div>
-            <Button variant='primary' onClick={container2Event}>Container 2</Button> {' '}
-          </div>
         </div>
         <div>
           <div>
             {data1}
-          </div>
-          <div>
-            {data2}
           </div>
         </div>
       </Container>
